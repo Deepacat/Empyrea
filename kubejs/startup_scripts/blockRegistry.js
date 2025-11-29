4a00b552a075f77f45e0a411b50b7454e8177b80
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-11-29 07:12:34 -0600

    add pulverizing catalyst

---
+++ kubejs/startup_scripts/blockRegistry.js
@@ -0,0 +1,7 @@
+StartupEvents.registry('block', e => {
+    e.create('kubejs:pulverizing_catalyst')
+        .soundType('stone')
+        .hardness(2)
+        .textureAll('botania:block/alchemy_catalyst')
+        .color('#44ecfb')
+})
