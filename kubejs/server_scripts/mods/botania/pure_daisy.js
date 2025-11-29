07b61f7427750f3220af1825b44945ed608a9eae
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-11-29 07:12:52 -0600

    add early dirt recipe

---
+++ kubejs/server_scripts/mods/botania/pure_daisy.js
@@ -0,0 +1,4 @@
+ServerEvents.recipes(e => {
+    e.recipes.botania.pure_daisy('minecraft:dirt', 'farmersdelight:organic_compost')
+        .id('kubejs:botania/pure_daisy/dirt')
+})
---
+++ kubejs/server_scripts/mods/farmersdelight/shapeless.js
@@ -0,0 +1,9 @@
+ServerEvents.recipes(e => {
+    e.remove({ output: 'farmersdelight:organic_compost' })
+    e.shapeless('farmersdelight:organic_compost', [
+        '3x minecraft:rotten_flesh', '2x farmersdelight:straw', '4x minecraft:bone_meal'
+    ]).id('kubejs:farmersdelight/shapeless/organic_compost_flesh')
+    e.shapeless('farmersdelight:organic_compost', [
+        '3x farmersdelight:straw', '2x minecraft:bone_meal', '4x farmersdelight:tree_bark'
+    ]).id('kubejs:farmersdelight/shapeless/organic_compost_bark')
+})
