b85c7441c848d76f54f3c237bfe0a8bb6133b8ac
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-12-05 05:15:11 -0600

    recipe qb tweaks

--- kubejs/server_scripts/mods/lychee/fluid_dipping.js
+++ kubejs/server_scripts/mods/lychee/fluid_dipping.js
@@ -6,4 +6,11 @@ ServerEvents.recipes(e => {
         Ingredient.of('minecraft:basalt').toJson(),
         "starbunclemania:source_fluid_block",
     ).id(`kubejs:lychee/fluid_dipping/deepslate_in_source`)
+
+    insideBlock(e, [
+        { type: "drop_item", item: "spectrum:mermaids_gem", count: 1 }
+    ],
+        Ingredient.of('minecraft:nautilus_shell').toJson(),
+        "spectrum:mud",
+    ).id(`kubejs:lychee/fluid_dipping/mermaids_gem_in_mud`)
 })
--- kubejs/server_scripts/mods/spectrum/pedestal_2_simple.js
+++ kubejs/server_scripts/mods/spectrum/pedestal_2_simple.js
@@ -1,5 +1,23 @@
 // tier 2 CMY upgraded pedestal recipes
 
 ServerEvents.recipes(e => {
-
+    e.custom({
+        "type": "spectrum:pedestal",
+        "time": 200, "tier": "simple", "experience": 1,
+        "magenta": 0, "yellow": 0, "cyan": 10, "black": 0, "white": 0,
+        "result": {
+            "item": 'spectrum:lagoon_rod',
+            "count": 1
+        },
+        "pattern": [
+            "  B",
+            " AC",
+            "B C"
+        ],
+        "key": {
+            "A": { "tag": 'kubejs:rare_mana' },
+            "B": { "item": 'spectrum:polished_basalt' },
+            "C": { "item": 'botania:mana_string' },
+        }
+    }).id('kubejs:spectrum/pedestal/lagoon_rod_mana')
 })
