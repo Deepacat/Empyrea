d8dd74b733cc74dbee934ec37db4cb6386bc3179
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-12-05 06:27:30 -0600

    retconned

--- kubejs/server_scripts/mods/ars_nouveau/shaped.js
+++ kubejs/server_scripts/mods/ars_nouveau/shaped.js
@@ -1,13 +1,3 @@
 ServerEvents.recipes(e => {
-    e.shaped('ars_nouveau:ritual_brazier', [
-        'ABA',
-        ' C ',
-        'DDD'
-    ], {
-        A: 'supplementaries:gold_trapdoor',
-        B: 'spectrum:amethyst_storage_block',
-        C: '#kubejs:rare_mana',
-        D: '#forge:ingots/gold'
-    }).id('kubejs:ars_nouveau/shaped/ritual_brazier')
 
 })
