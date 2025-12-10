e954578c6b1d5088e7481503fa12e54b9f9ed96c
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-12-10 04:01:52 -0600

    attractor model & states

--- kubejs/startup_scripts/astral_attractor/block.js
+++ kubejs/startup_scripts/astral_attractor/block.js
@@ -1,4 +1,7 @@
 // Meteor spawner block registry
+const $BooleanProperty = Java.loadClass("net.minecraft.world.level.block.state.properties.BooleanProperty")
+const meteor_spawned = $BooleanProperty.create("meteor_spawned")
+
 StartupEvents.registry("block", (e) => {
     e.create("kubejs:astral_attractor", "cardinal")
         .tagBlock("minecraft:mineable/pickaxe")
@@ -6,10 +9,12 @@ StartupEvents.registry("block", (e) => {
         .defaultCutout()
         .soundType("copper")
         .item(item => {
-            item.tooltip(Text.gray("Allures shimmering clusters to home in on it's position"))
+            item.tooltip(Text.gray("Allures shimmering clusters from above"))
+            item.tooltip(Text.gray("test"))
         })
-        .textureAll('awawatextureme')
-        // .model("minecraft:block/furnace")
+        .model("minecraft:block/furnace")
+        .property(meteor_spawned)
+        .defaultState(state => { state.cycle(meteor_spawned) })
         .blockEntity(blockInfo => {
             blockInfo.serverTick(20, 0, (entity) => {
                 global.attractorSpawnMeteor(entity)
