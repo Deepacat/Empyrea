3d9b49abc3254a6d08809520ebc6c5b98cd4b809
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-12-12 05:39:37 -0600

    remove plenty of ars essence recipes

--- kubejs/server_scripts/mods/recipeRemovals.js
+++ kubejs/server_scripts/mods/recipeRemovals.js
@@ -9,5 +9,18 @@ ServerEvents.recipes(e => {
     e.remove({ id: 'botania:mana_infusion/tropical_fish_to_pufferfish' })
 
     // mesh to obsidian
-    e.remove({ id: 'botania:pure_daisy/obsidian'})
-})
+    e.remove({ id: 'botania:pure_daisy/obsidian' })
+
+    // Ars skyblock essence recipe removals
+    e.remove({ id: 'ars_nouveau:conjuration_essence_to_end_stone' })
+    e.remove({ id: 'ars_nouveau:conjuration_essence_to_soul_sand' })
+    e.remove({ id: 'ars_nouveau:fire_essence_to_magma_block' })
+    e.remove({ id: 'ars_nouveau:manipulation_essence_to_andesite' })
+    e.remove({ id: 'ars_nouveau:manipulation_essence_to_calcite' })
+    e.remove({ id: 'ars_nouveau:manipulation_essence_to_deepslate' })
+    e.remove({ id: 'ars_nouveau:manipulation_essence_to_diorite' })
+    e.remove({ id: 'ars_nouveau:manipulation_essence_to_granite' })
+    e.remove({ id: 'ars_nouveau:manipulation_essence_to_tuff' })
+    e.remove({ id: 'minecraft:pointed_dripstone' }) // water essence and 4 stone from ars caelum, why mc namespace??
+
+})  
