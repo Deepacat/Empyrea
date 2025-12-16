44c7bcec5bdb65a6049fa735f132181b8652e7e4
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-12-16 16:06:59 -0600

    move scirps & edit fishing tables

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -1,6 +1,16 @@
 ServerEvents.tags('item', e => {
     e.add('kubejs:rare_mana', ['botania:mana_pearl', 'botania:mana_diamond'])
-    
+
     e.remove('essentials:circuit_components', ['minecraft:quartz'])
     e.add('essentials:circuit_components', ['malum:natural_quartz'])
+})
+
+ServerEvents.tags('block', e => {
+    e.add('minecraft:mineable/pickaxe', [
+        'schematicannon:schematicannon'
+    ])
+
+    e.add('minecraft:mineable/axe', [
+        'schematicannon:schematicannon'
+    ])
 })
