d7324e65e8cf2ea8bf1985eed92bf8e02754b6bf
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-12-16 21:10:33 -0600

    remove lagoon rod recipe since mermaids gems are fairly obtainable

--- kubejs/server_scripts/mods/spectrum/pedestal_2_simple.js
+++ kubejs/server_scripts/mods/spectrum/pedestal_2_simple.js
@@ -4,26 +4,6 @@ ServerEvents.recipes(e => {
     e.custom({
         "type": "spectrum:pedestal",
         "time": 200, "tier": "simple", "experience": 1,
-        "magenta": 0, "yellow": 0, "cyan": 10, "black": 0, "white": 0,
-        "result": {
-            "item": 'spectrum:lagoon_rod',
-            "count": 1
-        },
-        "pattern": [
-            "  B",
-            " AC",
-            "B C"
-        ],
-        "key": {
-            "A": { "tag": 'kubejs:rare_mana' },
-            "B": { "item": 'spectrum:polished_basalt' },
-            "C": { "item": 'botania:mana_string' },
-        }
-    }).id('kubejs:spectrum/pedestal/lagoon_rod_mana')
-
-    e.custom({
-        "type": "spectrum:pedestal",
-        "time": 200, "tier": "simple", "experience": 1,
         "magenta": 0, "yellow": 32, "cyan": 0, "black": 0, "white": 0,
         "result": {
             "item": 'minecraft:lava_bucket',
