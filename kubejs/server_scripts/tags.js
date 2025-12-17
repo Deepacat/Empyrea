97c17a99b2007595fddea7a76875a7dc8bc17478
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-12-16 21:08:52 -0600

    overhaul lava fishing tables

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -3,6 +3,9 @@ ServerEvents.tags('item', e => {
 
     e.remove('essentials:circuit_components', ['minecraft:quartz'])
     e.add('essentials:circuit_components', ['malum:natural_quartz'])
+
+    // lava rod wont give it immunity when fished for some reason
+    e.add('lychee:fire_immune', ['minecraft:netherrack'])
 })
 
 ServerEvents.tags('block', e => {
@@ -13,4 +16,9 @@ ServerEvents.tags('block', e => {
     e.add('minecraft:mineable/axe', [
         'schematicannon:schematicannon'
     ])
+})
+
+EntityEvents.spawned(e => {
+    if (e.entity.name.string != 'Netherrack') { return }
+    e.server.tell(`netherrack drop`)
 })
