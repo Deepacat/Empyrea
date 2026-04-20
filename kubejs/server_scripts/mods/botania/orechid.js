c23058d5acdeb96ebba12db7347058808f270dc3
Deepa <52133154+Deepacat@users.noreply.github.com>
2026-04-20 00:33:48 -0500

    add orechid ignem ores

--- kubejs/server_scripts/mods/botania/orechid.js
+++ kubejs/server_scripts/mods/botania/orechid.js
@@ -1,27 +1,53 @@
 ServerEvents.recipes(e => {
-    // amethyst/topaz/citrine ores from calcite
+    /* -- orechid calcite to amethyst / topaz / citrine-- */
+    // amethyst ore
     e.recipes.botania.orechid('spectrum:amethyst_ore', 'minecraft:calcite', 1) // spectrum amethyst ore
         .id('kubejs:botania/orechid/amethyst_ore')
+    // topaz ore
     e.recipes.botania.orechid('spectrum:topaz_ore', 'minecraft:calcite', 1) // spectrum topaz ore
         .id('kubejs:botania/orechid/topaz_ore')
+    // citrine ore
     e.recipes.botania.orechid('spectrum:citrine_ore', 'minecraft:calcite', 1) // spectrum citrine ore
         .id('kubejs:botania/orechid/citrine_ore')
 
-    // magic ores from sourcestone
-    e.recipes.botania.orechid('malum:deepslate_soulstone_ore', 'ars_nouveau:sourcestone', 1) // malum soulstone 
+    /* -- orechid sourcestone to magic ores */
+    // malum soulstone
+    e.recipes.botania.orechid('malum:deepslate_soulstone_ore', 'ars_nouveau:sourcestone', 1)
         .id('kubejs:botania/orechid/deepslate_soulstone_ore')
-    e.recipes.botania.orechid('malum:brilliant_deepslate', 'ars_nouveau:sourcestone', 1) // malum brilliant
+    // malum brilliant ore
+    e.recipes.botania.orechid('malum:brilliant_deepslate', 'ars_nouveau:sourcestone', 1)
         .id('kubejs:botania/orechid/brilliant_deepslate')
-    e.recipes.botania.orechid('malum:natural_quartz_ore', 'ars_nouveau:sourcestone', 2) // malum natural quartz
+    // malum natural quartz
+    e.recipes.botania.orechid('malum:natural_quartz_ore', 'ars_nouveau:sourcestone', 2)
         .id('kubejs:botania/orechid/natural_quartz_ore')
-    e.recipes.botania.orechid('malum:cthonic_gold_ore', 'ars_nouveau:sourcestone', 1) // malum cthonic gold
+    // malum cthonic gold
+    e.recipes.botania.orechid('malum:cthonic_gold_ore', 'ars_nouveau:sourcestone', 1)
         .id('kubejs:botania/orechid/cthonic_gold_ore')
-    e.recipes.botania.orechid('eidolon:deep_silver_ore', 'ars_nouveau:sourcestone', 1) // eidolon silver
+    // eidolon silver
+    e.recipes.botania.orechid('eidolon:deep_silver_ore', 'ars_nouveau:sourcestone', 1)
         .id('kubejs:botania/orechid/deep_silver_ore')
-    e.recipes.botania.orechid('eidolon:deep_lead_ore', 'ars_nouveau:sourcestone', 1) // eidolon lead
+    // eidolon lead
+    e.recipes.botania.orechid('eidolon:deep_lead_ore', 'ars_nouveau:sourcestone', 1)
         .id('kubejs:botania/orechid/deep_lead_ore')
-    e.recipes.botania.orechid('spectrum:deepslate_azurite_ore', 'ars_nouveau:sourcestone', 1) // spectrum azurite
+    // spectrum azurite
+    e.recipes.botania.orechid('spectrum:deepslate_azurite_ore', 'ars_nouveau:sourcestone', 1)
         .id('kubejs:botania/orechid/deepslate_azurite_ore')
 
-    // orechid ignem netherrack ores
+    /* -- orechid ignem ores */
+    // netherrack hidden iesnium from netherrack
+    e.recipes.botania.orechid_ignem('occultism:iesnium_ore_natural', 'netherrack', 100)
+        .id('kubejs:botania/orechid/netherrack_natural_iesnium')
+    // nether salt from netherrack
+    e.recipes.botania.orechid_ignem('wizards_reborn:nether_salt_ore', 'netherrack', 100)
+        .id('kubejs:botania/orechid/nether_salt_ore')
+    // blazing quartz from vanilla quartz ore
+    e.recipes.botania.orechid_ignem('malum:blazing_quartz_ore', 'minecraft:nether_quartz_ore', 33)
+        .id('kubejs:botania/orechid/blazing_quartz_ore_nether')
+    e.recipes.botania.orechid_ignem('minecraft:netherrack', 'minecraft:nether_quartz_ore', 67)
+        .id('kubejs:botania/orechid/nether_quartz_loss')
+    // stratine from nether gold ore
+    e.recipes.botania.orechid_ignem('spectrum:stratine_ore', 'minecraft:nether_gold_ore', 5)
+        .id('kubejs:botania/orechid/stratine_ore')
+    e.recipes.botania.orechid_ignem('minecraft:netherrack', 'minecraft:nether_gold_ore', 95)
+        .id('kubejs:botania/orechid/nether_gold_loss')
 })
