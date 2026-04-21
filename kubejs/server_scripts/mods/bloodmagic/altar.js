fad2419c946e51ca0ffd04fd5e82960fdb2f1e20
Deepa <52133154+Deepacat@users.noreply.github.com>
2026-04-21 07:03:09 -0500

    felon recipe

---
+++ kubejs/server_scripts/mods/bloodmagic/altar.js
@@ -0,0 +1,8 @@
+ServerEvents.recipes(e => {
+    e.recipes.bloodmagic.altar('kubejs:felon', 'spectrum:glistering_melon')
+        .upgradeLevel(0)
+        .altarSyphon(2000)
+        .consumptionRate(5)
+        .drainRate(1)
+        .id('kubejs:mods/bloodmagic/altar/felon')
+})
