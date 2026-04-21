981b47f09508c8bb6a53c153fc072b2ac2d6fefc
Deepa <52133154+Deepacat@users.noreply.github.com>
2026-04-21 06:36:42 -0500

    convicted felon

--- kubejs/startup_scripts/blockRegistry.js
+++ kubejs/startup_scripts/blockRegistry.js
@@ -1,6 +1,27 @@
+const $FelPumpkinBlock = Java.loadClass('vazkii.botania.common.block.FelPumpkinBlock')
+const $Properties = Java.loadClass('net.minecraft.world.level.block.state.BlockBehaviour$Properties')
+const $Blocks = Java.loadClass('net.minecraft.world.level.block.Blocks')
+const $BlockItem = Java.loadClass('net.minecraft.world.item.BlockItem')
+const $IProperties = Java.loadClass('net.minecraft.world.item.Item$Properties')
+
+let felonBlock
+
 StartupEvents.registry('block', e => {
     e.create('kubejs:pulverizing_catalyst')
         .soundType('stone')
         .tagBlock('minecraft:mineable/pickaxe')
         .hardness(2)
+
+    // Fel melon block
+    felonBlock = e.createCustom('kubejs:felon',
+        () => new $FelPumpkinBlock(new $Properties.copy($Blocks.MELON))
+    ).tagBlock('minecraft:mineable/axe')
+})
+
+// Fel melon item 
+StartupEvents.registry('item', e => {
+    e.createCustom(
+        'kubejs:felon',
+        () => new $BlockItem(felonBlock.get(), new $IProperties())
+    )
 })
